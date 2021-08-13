856563ab893944cdceeb245188199ad3cf13f438
Spyro <epicdudeness677@gmail.com>
2021-08-13 20:08:39 +0200

    Implemented a primative gravity flip ability.

--- Assets/Scripts/Player/Abilities/GravityMod.cs
+++ Assets/Scripts/Player/Abilities/GravityMod.cs
@@ -3,6 +3,7 @@ using NotImplementedException = System.NotImplementedException;
 
 namespace MajorJam.System
 {
+    [CreateAssetMenu(menuName = "Cheats/Gravity Mod", fileName = "New Gravity Modifier", order = 0)]
     public class GravityMod : Ability
     {
         public enum GravityDirection
@@ -17,15 +18,26 @@ namespace MajorJam.System
         {
             if (isUsable)
             {
+                Transform worldPivot = GameObject.FindGameObjectWithTag("World Pivot").transform;
+
+                worldPivot.transform.rotation = gravityDirection == GravityDirection.Up
+                    ? Quaternion.Euler(0, 0, 180)
+                    : Quaternion.identity;
+
+                playerController.transform.parent.localScale =
+                    gravityDirection == GravityDirection.Up ? new Vector3(1, -1, 1) : Vector3.one;
+
                 Physics.gravity = new Vector3(Physics.gravity.x,
                     gravityDirection == GravityDirection.Up
-                        ? -Mathf.Abs(Physics.gravity.y)
-                        : Mathf.Abs(Physics.gravity.y), Physics.gravity.z);
-                isUsable = false;
+                        ? Mathf.Abs(Physics.gravity.y)
+                        : -Mathf.Abs(Physics.gravity.y), Physics.gravity.z);
+             
 
                 gravityDirection = gravityDirection == GravityDirection.Up
                     ? GravityDirection.Down
                     : GravityDirection.Up;
+                
+                isUsable = false;
             }
         }
     }
