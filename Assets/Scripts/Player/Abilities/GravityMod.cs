0344a2f5199244922e710defc144d564dffb9dff
Spyro <epicdudeness677@gmail.com>
2021-08-13 18:30:24 +0200

    Updated CheatCode System to be more responsive and less punishing upon misstyping a code wrong.

---
+++ Assets/Scripts/Player/Abilities/GravityMod.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using NotImplementedException = System.NotImplementedException;
+
+namespace MajorJam.System
+{
+    public class GravityMod : Ability
+    {
+        public enum GravityDirection
+        {
+            Down,
+            Up
+        }
+
+        public GravityDirection gravityDirection;
+
+        protected override void OnAbilityUse(PlayerController playerController)
+        {
+            if (isUsable)
+            {
+                Physics.gravity = new Vector3(Physics.gravity.x,
+                    gravityDirection == GravityDirection.Up
+                        ? -Mathf.Abs(Physics.gravity.y)
+                        : Mathf.Abs(Physics.gravity.y), Physics.gravity.z);
+                isUsable = false;
+
+                gravityDirection = gravityDirection == GravityDirection.Up
+                    ? GravityDirection.Down
+                    : GravityDirection.Up;
+            }
+        }
+    }
+}
